f1aed9c6507b65549f5610326d8d7e6f1ee2a5d3
Miguel Silva <miguel.silva.422@gmail.com>
2022-04-08 21:21:06 +0100

    added listening func to test server

--- src/main.js
+++ src/main.js
@@ -7,4 +7,4 @@ app.get('/', (req, res) => {
   res.send('Test route')
 })
 
-app.listen()
+app.listen(process.env.WEB_PORT, () => { console.log(`Server listenning: ${process.env.WEB_PROTOCOL}//${process.env.WEB_DOMAIN}:${process.env.WEB_PORT}`) })
